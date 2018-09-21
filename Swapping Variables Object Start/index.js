53fbe65b20efd1dc66819e8ec2d5bf7413d83a89
Brayden Sakovich <b.sakovich@share.epsb.ca>
2018-09-21 13:54:18 -0600

    many things

---
+++ Swapping Variables Object Start/index.js
@@ -0,0 +1,30 @@
+console.log('Hello World');
+
+//Create 10 variables about yourself
+let BraydenSakovich = {
+  firstName: "Brayden",
+  lastName: "Sakovich",
+  age: 16,
+  ageAfraid: null,
+  hair: true,
+  male: true,
+  female: false,
+}
+
+let MeganSpence = {
+  firstName: "Megan",
+  lastName: "Spence",
+  age: 14,
+  ageAfraid: null,
+  hair: true,
+  male: false,
+  female: true,
+}
+
+//print entire object
+console.log(BraydenSakovich);
+console.log(MeganSpence);
+
+//print first names
+console.log(BraydenSakovich.firstName);
+console.log(MeganSpence.firstName);
--- Swapping Variables/index.js
+++ Swapping Variables/index.js
@@ -2,7 +2,11 @@ let firstName = "Brayden";
 let lastName = "Sakovich";
 
 //new code goes here
+//Add a third variable
+let memory = firstName;
+firstName = lastName;
+lastName = memory;
 
 //Print variables to console
-console.log(firstName);
-console.log(lastName);
+console.log("My first name is: " + firstName);
+console.log("My last name is: " + lastName);
