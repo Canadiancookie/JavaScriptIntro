034347e2a32b1ece92b6626e1f0b042352096411
Brayden Sakovich <b.sakovich@share.epsb.ca>
2018-09-21 13:01:34 -0600

    swappingvariables index.js + space in scriptsrc

--- Swapping Variables/index.js
+++ Swapping Variables/index.js
@@ -1,18 +1,8 @@
-console.log('Hello World');
-
-//Create 10 variables about yourself
 let firstName = "Brayden";
 let lastName = "Sakovich";
-let age = 16;
-const ageAfraid = 29; //security feature
-let hair = null; //undefined, null
-//debug with typeof
-let male = true;
+
+//new code goes here
 
 //Print variables to console
 console.log(firstName);
 console.log(lastName);
-console.log(age);
-console.log(ageAfraid);
-console.log(hair);
-console.log("Am I male?" + male);
