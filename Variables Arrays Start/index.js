6f3f2de278184e86d120a364811b0a31d8311291
Brayden Sakovich <b.sakovich@share.epsb.ca>
2018-09-24 12:53:12 -0600

    Variables Arrays Start

---
+++ Variables Arrays Start/index.js
@@ -0,0 +1,18 @@
+console.log('Hello World');
+
+//Create 10 variables about yourself
+let firstName = "Brayden";
+let lastName = "Sakovich";
+let age = 16;
+const ageAfraid = 29; //security feature
+let hair = null; //undefined, null
+//debug with typeof
+let male = true;
+
+//Print variables to console
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(ageAfraid);
+console.log(hair);
+console.log("Am I male?" + male);
